26191582f070ad8d692a457311f45bc4d34ce601
someonenevercry2025-oss <someonenevercry2025@gmail.com>
2026-02-23 21:25:57 +0800

    Add files via upload

--- src/models/Player.js
+++ src/models/Player.js
@@ -1,1 +1,15 @@
+const mongoose = require("mongoose");
 
+const playerSchema = new mongoose.Schema({
+  playerId: { type: String, unique: true },
+  username: { type: String, default: "Guest" },
+  coins: { type: Number, default: 100 },
+  hints: { type: Number, default: 3 },
+  hearts: { type: Number, default: 5 },
+  lastHeartUpdate: { type: Date, default: Date.now },
+  streak: { type: Number, default: 0 },
+  totalWin: { type: Number, default: 0 },
+  totalLose: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model("Player", playerSchema);
